9e99098b19f2a677173d3e7da796e3034fb1cdd2
Kostya20292 <78802135+Kostya20292@users.noreply.github.com>
2021-07-20 17:38:47 +0300

    Сверстан полный сайт с адаптивом

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(window).load(function () {
+      $(".before-after").twentytwenty({
+         before_label: 'Без скинали',
+         after_label: 'Со скинали' 
+      });
+      $('.before-slider').slick({
+        draggable: false,
+        dots: true,
+        dotsClass: 'before-slider__dots',
+        prevArrow: $('.arrow-left'),
+        nextArrow: $('.arrow-right')
+      });
+      $('.menu-button').on('click', function(){
+        $('.menu').toggleClass('menu_active');
+      });
+
+      /* Настройка select */
+      $('.select_checked').on('click', function(){
+        $('.select__dropdown').toggleClass('select__dropdown_open');
+      });
+      $('.select__option').on('click', function(){
+       var value = $(this).attr('data-value');
+       $('#select-type').val(value);
+       $('.select_checked').text(value);
+       $('.select__dropdown').toggleClass('select__dropdown_open');
+      });
+      $("a[href^='#']").click(function(){
+        var _href = $(this).attr('href');
+        $("html, body").animate({scrollTop: $(_href).offset().top -130 + "px"});
+        return false;
+      });
+      $('input[type="tel"]').mask("+375 (99) 999-99-99")
+      /* Показывать карту только когда докрутили до нее */ 
+      var reviews = $('.reviews');
+      var reviewsTop = reviews.offset().top;
+      $(window).bind('scroll', function(){
+        var windowTop = $(this).scrollTop();
+        if (windowTop > reviewsTop) {
+          $('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A305aa8f2b9c94d215abe8bbf3a178c1f3f8fafe6dfc99abbbe1d4095095b9a8b&amp;width=100%25&amp;height=410&amp;lang=ru_RU&amp;scroll=false"></script>')
+          $(window).unbind('scroll')
+        }
+      });
+    });
